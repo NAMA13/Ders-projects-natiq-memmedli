f5f3ffac5e10080115271f3bf5220e1399e2583f
KH <natiq.09.az@gmail.com>
2022-04-27 22:06:15 +0400

    Pasword Generetor (Vers - 1.3)
    Yeni bir Pasword duzelder (Rndom olaraq)

--- javase/src/com/company/password_generetor.java
+++ javase/src/com/company/password_generetor.java
@@ -9,7 +9,7 @@ public class password_generetor {
 
     static void generator() {
         Random r = new Random();
-        String password = "";
+        StringBuilder password = new StringBuilder();
 
         String[] spacarc = {
                 "?", "!", "@", "#", "$", "%", "^"
@@ -19,15 +19,14 @@ public class password_generetor {
         for (int i = 0; i < sequence.length; i++){
             sequence[i] = (int) ((Math.random() * (4 - 1)) + 1);
         }
-            char hrf = 'a';
 
         for (int k : sequence) {
             if (k == 1) {
-                password += (int) ((Math.random() * (9 - 1)) + 1);
+                password.append((int) ((Math.random() * (9 - 1)) + 1));
             } else if (k == 2) {
-                password += spacarc[(int) ((Math.random() * (spacarc.length - 1)) + 1)];
+                password.append(spacarc[(int) ((Math.random() * (spacarc.length - 1)) + 1)]);
             } else if (k == 3) {
-                password += (char)(r.nextInt(26) + 'a');
+                password.append((char) (r.nextInt(26) + 'a'));
             }
 //            System.out.println(password);
         }
