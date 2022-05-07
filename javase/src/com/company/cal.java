e752016d68ed7f51f275c4c54c85ede909bbac6f
KH <natiq.09.az@gmail.com>
2022-05-07 14:10:49 +0400

    Calisma

---
+++ javase/src/com/company/cal.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+public class Tap9 {
+    Scanner sn = new Scanner(System.in);
+    System.out.print("Enter Final Number: ");
+    int count = sn.nextInt();
+    int n1 = 0, n2 = 1, n3;
+    System.out.print(n1+" "+n2);
+
+ for( int i = 2; i < count ; i++ )
+    {
+        n3 = n1 + n2;
+        System.out.print(" " + n3);
+        n1 = n2;
+        n2 = n3;
+    }
+}
