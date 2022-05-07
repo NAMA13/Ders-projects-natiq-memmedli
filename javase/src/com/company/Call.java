e3aeb2e662f8a9815645d830eef1c132dbb56823
KH <natiq.09.az@gmail.com>
2022-05-07 14:26:31 +0400

    Call

--- javase/src/com/company/Call.java
+++ javase/src/com/company/Call.java
@@ -6,10 +6,12 @@ public class Call {
     public static void main(String[] args) {
         int agac = 22;
         double versu = 1;
-        while(agac > 30){
+        while(agac < 30){
             double rol = 2 * versu;
             agac+= rol;
             versu = versu * 1.5;
+            System.out.println("Versu; " + versu);
+            System.out.println("Agac; " + agac);
         }
     }
 }
