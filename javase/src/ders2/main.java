21f71edf3e12fb21b168e9e6bb02c979a1c0b1d6
KH <natiq.09.az@gmail.com>
2022-06-05 12:06:15 +0400

    Ders

---
+++ javase/src/ders2/main.java
@@ -0,0 +1,11 @@
+package ders2;
+
+import java.time.LocalDate;
+
+public class main {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date.getMonth());
+
+    }
+}
---
+++ javase/src/ders2/person.java
@@ -0,0 +1,9 @@
+package ders2;
+
+import java.time.LocalDate;
+
+public class person {
+    int id = 0;
+    String name;
+    LocalDate birthday;
+}
