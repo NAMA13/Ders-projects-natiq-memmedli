9688c1ea5c11aea8eb943af1272793f961d4663b
NAMA13 <natiq.09.az@gmail.com>
2022-11-27 12:00:50 +0400

    064 Spring MVC Html working

---
+++ spring/spring-mvc/spring-mvc/src/main/java/az/developia/springmvc/StudentController.java
@@ -0,0 +1,13 @@
+package az.developia.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudentController {
+	
+	@GetMapping
+	public String showPage() {
+		return "students";
+	}
+}
