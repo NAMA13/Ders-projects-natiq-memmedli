fe8631b77ec07004ded7e8d92903aaa04ab4e4d5
NAMA13 <natiq.09.az@gmail.com>
2023-02-24 23:38:02 +0400

    Author, PageCount, Image added (16 - 18)

--- spring/new-project/CourseM/CourseM/src/main/java/com/developia/CourseM/Controller/BookController.java
+++ spring/new-project/CourseM/CourseM/src/main/java/com/developia/CourseM/Controller/BookController.java
@@ -3,6 +3,9 @@ package com.developia.CourseM.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,25 +17,38 @@ import com.developia.CourseM.Repository.BookRepository;
 
 @Controller
 public class BookController {
-	@Autowired 
+
+	public static String getUsername() {
+		String currentUserName = null;
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+			currentUserName = authentication.getName();
+		}
+		return currentUserName;
+	}
+
+	@Autowired
 	private BookRepository repository;
-	
-	@GetMapping(path="/books/findAll")
+
+	@GetMapping(path = "/books/findAll")
 	public String showHomePage(Model model) {
 		List<Book> books = repository.findAll();
 		model.addAttribute("books", books);
 		return "books";
 	}
-	
-	@GetMapping(path="/books/new")
+
+	@GetMapping(path = "/books/new")
 	public String openNewBookPage(Model model) {
 		Book book = new Book();
 		model.addAttribute("book", book);
 		return "new-book";
 	}
-	
-	@PostMapping(path="/books/new-book-process")
-	public String saveBook(@ModelAttribute(name="book")Book book, Model model) {
+
+	@PostMapping(path = "/books/new-book-process")
+	public String saveBook(@ModelAttribute(name = "book") Book book, Model model) {
+		book.setImage("Book.jpg");
+
+		book.setUsername(getUsername());
 		repository.save(book);
 		List<Book> books = repository.findAll();
 		model.addAttribute("books", books);
--- spring/new-project/CourseM/CourseM/src/main/java/com/developia/CourseM/config/SecurityConfig.java
+++ spring/new-project/CourseM/CourseM/src/main/java/com/developia/CourseM/config/SecurityConfig.java
@@ -1,47 +1,49 @@
-//package com.developia.CourseM.config;
-//
-//import javax.sql.DataSource;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	
-//	@Autowired
-//	private DataSource ds;
-//	
-////	@Override
-////	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-////		 auth.jdbcAuthentication().dataSource(ds);
-////	}
-//	
+package com.developia.CourseM.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	@Autowired
+	private DataSource ds;
+	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		 auth.jdbcAuthentication().dataSource(ds);
+//	}
+	
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
+//
 //
 //		http.authorizeRequests()
 //		.antMatchers(HttpMethod.GET, "/h2-console/**").permitAll()
 //		.antMatchers(HttpMethod.GET, "/css/**").permitAll()
 //		.antMatchers(HttpMethod.GET, "/js/**").permitAll()
 //		.anyRequest().authenticated();
-//
+//		}
 //
 //	}
-//	
-//	
-//	
-//}
-//
-//
-//
-//
-//
+
+}
+
+
+
+
+
